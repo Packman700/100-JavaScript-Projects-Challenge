124d9fcc9ef0fe1362a48ba3a3891a7a6b234a67
Packman700 <53169727+Packman700@users.noreply.github.com>
2021-02-28 16:02:19 +0100

    [fix] Fix bug occurring when program generate one digit hex number

--- 1. Change Background Color Project/app.js
+++ 1. Change Background Color Project/app.js
@@ -5,9 +5,9 @@ function changeBackgroundColor(){
 }
 
 function returnRandomHexColor(){
-    const hexR = returnRandomNumber().toString(16);
-    const hexG = returnRandomNumber().toString(16);
-    const hexB = returnRandomNumber().toString(16);
+    const hexR = formatToTwoDigits(returnRandomNumber().toString(16));
+    const hexG = formatToTwoDigits(returnRandomNumber().toString(16));
+    const hexB = formatToTwoDigits(returnRandomNumber().toString(16));
     return `#${hexR}${hexG}${hexB}`;
 }
 
@@ -15,4 +15,18 @@ function returnRandomNumber(min= 0, max = 255){
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function formatToTwoDigits(inputNumber) {
+    /*
+        intUnitTime is Hour, Minute or Second number
+        This function convert numbers to 2 digits strings
+        Input: 1   Output: "01"
+        Input: 21  Output: "21"
+    */
+    const stringTime = inputNumber.toString();
+    if (stringTime.length === 2)
+        return stringTime;
+    else
+        return `0${stringTime}`;
 }
